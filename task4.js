e550adcd2573eed7c6add505f5735163e99b1433
wisanggenib <bagus.wisanggeni4897@gmail.com>
2021-01-22 08:52:33 +0700

    Fix no 4

--- task4.js
+++ task4.js
@@ -5,8 +5,9 @@ const getData = async () => {
     let response = await fetch('https://mul14.github.io/data/employees.json');
     if (response.ok) { // if HTTP-status is 200-299
         // get the response body (the method explained below)
-         data = await response.json();
+        data = await response.json();
         // console.log(data[0].salary)
+        console.log(data)
 
 
     } else {
@@ -32,8 +33,48 @@ const getAddress = async () => {
     for (let i = 0; i < data.length; i++) {
         let search = data[i].addresses[0].city
         if (search.includes("Jakarta") === true) {
-            console.log((i + 1) + " - " +data[i].first_name + " Live in " + data[i].addresses[0].city)
+            console.log((i + 1) + " - " + data[i].first_name + " Live in " + data[i].addresses[0].city)
         }
+    }
+    console.log("=========================")
+}
+
+const getBirthday = async () => {
+    //getData()
+    console.log("Employer born in April - No 3")
+    for (let i = 0; i < data.length; i++) {
+        let search = data[i].birthday
+        if (search.includes("-04-") === true) {
+            console.log((i + 1) + " - " + data[i].first_name + " Was Born " + data[i].birthday)
+        }
+    }
+    console.log("=========================")
+}
+
+const getDepartment = async () => {
+    //getData()
+    console.log("Employer That work at Research and development - No 4")
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].department.name === "Research and development") {
+            console.log((i + 1) + " - " + data[i].first_name + " Work On " + data[i].department.name)
+        }
+    }
+    console.log("=========================")
+}
+
+const getAbsence = async () => {
+    //getData()
+    let total = 0 
+    console.log("Employer That Absence -  No 5")
+    for (let i = 0; i < data.length; i++) {
+        for (let j = 0; j < data[i].presence_list.length; j++) {
+            let search = data[i].presence_list[j]
+            if (search.includes("-10-") === true) {
+                total += 1
+            }
+        }
+        console.log((i + 1) + " - " + data[i].first_name + " Was Absence = " + total + " Times")
+        total = 0
     }
     console.log("=========================")
 }
@@ -42,6 +83,9 @@ const getAnswer = async () => {
     const A = await getData()
     const B = await getMaxSalary()
     const C = await getAddress()
+    const D = await getBirthday()
+    const E = await getDepartment()
+    const F = await getAbsence()
 }
 
 getAnswer()
